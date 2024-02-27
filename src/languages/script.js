09b37b40323d61512b8d5e70647bc6ec30c53a88
CuB1z <cub1zdev@gmail.com>
2024-02-27 23:59:07 +0100

    Add ChangeLanguage component and en.svg flag

--- src/languages/script.js
+++ src/languages/script.js
@@ -2,6 +2,7 @@ import spanish from './es.json'
 import english from './en.json'
 
 const LANGUAGES = {
+    default: "",
     en: "en",
     es: "es"
 }
@@ -10,6 +11,18 @@ const getLanguage = (locale) => {
     if (locale === LANGUAGES.en) return english
     if (locale === LANGUAGES.es) return spanish
     return english
+}
+
+export const getActualLanguage = (locale) => {
+    if (locale === LANGUAGES.en) return LANGUAGES.en
+    if (locale === LANGUAGES.es) return LANGUAGES.es
+    return LANGUAGES.en
+}
+
+export const getAlternativeLanguage = (locale) => {
+    if (locale === LANGUAGES.en) return LANGUAGES.es
+    if (locale === LANGUAGES.es) return LANGUAGES.default
+    return LANGUAGES.default
 }
 
 export default getLanguage
